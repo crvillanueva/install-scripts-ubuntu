62c55fb033920e6b2d0c7576ed242b7a058b4fae
crvillanueva <cristobaljvp@gmail.com>
2022-09-19 17:58:45 -0300

    corregir quotes en gsettings y agregar keybidings

--- install-go.sh
+++ install-go.sh
@@ -1,5 +1,9 @@
-wget https://go.dev/dl/go1.19.1.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.19.1.linux-amd64.tar.gz
+GO_BINARY_URL='https://go.dev/dl/go1.19.1.linux-amd64.tar.gz'
+GO_BINARY_NAME=$(basename $GO_BINARY_URL)
+
+wget $GO_BINARY_URL
+sudo tar -C /usr/local -xzf $GO_BINARY_NAME
+rm $GO_BINARY_NAME
 
 # Add Go to the PATH
 echo '# Go Binaries to PATH' >> ~/.bashrc
