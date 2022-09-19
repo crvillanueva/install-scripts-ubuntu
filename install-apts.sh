8e1eca015af7ff29f6fe4e9c198438fadbc1862a
crvillanueva <cristobaljvp@gmail.com>
2022-09-19 17:04:30 -0300

    agregar wireguard

--- install-apts.sh
+++ install-apts.sh
@@ -1,6 +1,6 @@
 sudo apt update -y && sudo apt upgrade -y
 
-APTS_LIST=("bat" "exa" "duf" "neofetch")
+APTS_LIST=("bat" "exa" "duf" "neofetch wireguard")
 
 for app in ${APTS_LIST[@]}; do
   sudo apt install $app -y
