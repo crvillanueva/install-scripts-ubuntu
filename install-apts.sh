152d1f68ccea81ea6104cb8961a6ee30345f0338
crvillanueva <cristobaljvp@gmail.com>
2022-09-19 17:17:44 -0300

    agregar neovim

--- install-apts.sh
+++ install-apts.sh
@@ -1,6 +1,6 @@
 sudo apt update -y && sudo apt upgrade -y
 
-APTS_LIST=("bat" "exa" "duf" "neofetch wireguard")
+APTS_LIST=("bat" "exa" "duf" "neofetch" "wireguard" "neovim")
 
 for app in ${APTS_LIST[@]}; do
   sudo apt install $app -y
