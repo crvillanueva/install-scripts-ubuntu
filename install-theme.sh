3055c155e3789af6fc38efc2128ff625c9ada1f1
crvillanueva <cristobaljvp@gmail.com>
2022-09-19 18:26:41 -0300

    instalacion no interactiva de tema gnome terminal

--- install-theme.sh
+++ install-theme.sh
@@ -2,5 +2,7 @@
 
 sudo apt-get install dconf-cli git -y
 git clone https://github.com/dracula/gnome-terminal
-cd gnome-terminal
-./install.sh
+cd gnome-terminal \
+    && ./install.sh --scheme=Dracula --profile=Default --skip-dircolors \
+    && cd .. \
+    && rm -rf gnome-terminal
