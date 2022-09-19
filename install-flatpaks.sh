63bb7c80b821583115ed52cff46c972ae1ec2571
crvillanueva <cristobaljvp@gmail.com>
2022-09-19 20:55:58 -0300

    corregir flatpak repo

--- install-flatpaks.sh
+++ install-flatpaks.sh
@@ -1,5 +1,7 @@
+# add flatpak remote
+flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
 flatpak install flathub org.mozilla.firefox -y
 # install remmina 
-flatpak install org.freedesktop.Platform -y
-flatpak install --user flathub org.mozilla.Remmina -y
+flatpak install --user flathub org.remmina.Remmina -y
 flatpak install --user flathub io.bassi.Amberol -y
--- pre-install.sh
+++ pre-install.sh
@@ -10,6 +10,7 @@ sudo apt update
 sudo apt install flatpak -y
 # add the flathub repository
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # install deb-get
 curl -sL https://raw.githubusercontent.com/wimpysworld/deb-get/main/deb-get | sudo -E bash -s install deb-get
