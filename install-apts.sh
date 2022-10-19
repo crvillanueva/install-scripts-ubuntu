56268a4f7a922344a6f5cc869cd1b423147a6333
Cristobal Villanueva <cristobaljvp@gmail.com>
2022-10-19 16:04:45 -0300

    feat: agregar zsh, ripgrep y trash-cli a apts

--- install-apts.sh
+++ install-apts.sh
@@ -1,7 +1,9 @@
 sudo apt update -y && sudo apt upgrade -y
 
-APTS_LIST=("bat" "exa" "duf" "neofetch" "wireguard" "neovim" "filezilla")
+APTS_LIST=("bat" "exa" "duf" "neofetch" "wireguard" \
+    "neovim" "filezilla" "zsh" "ripgrep" "trash-cli")
 
 for app in ${APTS_LIST[@]}; do
   sudo apt-get install $app -y
 done
+
